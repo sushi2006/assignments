bc7988cbeb2da79db07ce4a5ee562c7beb9a589f
sushi2006 <94754767+sushi2006@users.noreply.github.com>
2021-11-27 10:56:22 -0600

    my second assignment

---
+++ 19_hw2.py
@@ -0,0 +1,7 @@
+# Write program to Find the perimeter of the rectangle
+
+a=c=2 
+b=d=4   
+Perimeter = 2*(a+ b)  
+print("Perimeter of rectangle is: ");  
+print(Perimeter);  
---
+++ 20_hw2.py
@@ -0,0 +1,9 @@
+# Write program to Find the perimeter of the triangle
+
+a = float(input("Enter the length of the first side in cm : "))
+b = float(input("Enter the length of the second side in cm : "))
+c = float(input("Enter the length of the third side in cm : "))
+
+perimeter = a + b + c
+
+print("The perimeter of the triangle is {} ".format(perimeter))
